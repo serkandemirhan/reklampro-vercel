1b21522b23dc37160c1fb72db34f1b63ea8247f8
serkandemirhan <serkandemirhan@gmail.com>
2025-10-05 17:05:10 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,4 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
-  }
-)
+// lib/supabase.ts
+'use client';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+export const supabase = createClientComponentClient();
