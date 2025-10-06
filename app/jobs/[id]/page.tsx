534b864ec6fddb86b68aa008183aff4a4bb921d8
serkandemirhan <serkandemirhan@gmail.com>
2025-10-06 20:54:06 +0200

    Update page.tsx

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,4 +1,18 @@
-import StepsTable from './StepsTable'
+import StepsTable from './StepsTable'// dosyanın üstüne ekleyin
+import AssignUserCell from './AssignUserCell'
+
+// ... tablo başlıklarında:
+<th>Sorumlu</th>
+
+// ... satır renderında (s: adım kaydı)
+<td>
+  <AssignUserCell
+    stepId={s.id}
+    value={s.assigned_user /* varsa kolon adını uyarlayın */}
+    onSaved={refetchSteps /* varsa listeyi yenileyen fonksiyonunuz */}
+  />
+</td>
+
 
 // ...
 export default async function JobDetailPage({ params }: { params: { id: string } }) {
